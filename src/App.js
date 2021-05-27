da72a8b56e118f0781d3ee541b76c342a8e9be52
Han Seung Joo <gkstmdwn234@gmail.com>
2021-05-27 15:04:50 +0900

    almost fin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+import Movie from "./movie";
+import "./App.css";
 
 export default class App extends React.Component {
   // 꼭 여기에 state 전부 선언할 필요 없음. 나중에 다른곳에서 그냥 추가해도됨.
@@ -6,17 +9,36 @@ export default class App extends React.Component {
     isLoading: true,
     movies: []
   };
+
+  getMovies = async () => {
+    // api 사이트에 보면 sort_by명령어 등등 정렬같은거 나옴. 뒤에?sort by=방식 쓰면댐
+    const {data: {data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log(movies);
+    this.setState({movies, isLoading:false})
+    // 바로윗줄 movies는 movies:movies로 state movie: axios movies가 단축된것.
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    },6000);
+    // setTimeout(() => {
+    //   this.setState({isLoading : false});
+    // },6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading} = this.state;
+    const { isLoading,movies} = this.state;
     return (
-      <React.Fragment>
-        <div>{isLoading ? "Loading" : "We are ready"}</div>
-      </React.Fragment>
+        <section className="container">
+          {isLoading ? (
+            <div className="loader">
+            <span className="Loader__tect">Loading...</span>
+          </div>) : (
+            <div className="movies">
+              {movies.map(movie => (
+                // console.log(movies);
+                    <Movie  key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}></Movie>
+                  ))}
+            </div>
+          )}
+        </section>
     )
   }
 }
