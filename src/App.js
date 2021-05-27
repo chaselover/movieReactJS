8af2f6c749fb810e20ca04d180df5f9d4bcf7654
Han Seung Joo <gkstmdwn234@gmail.com>
2021-05-27 11:04:00 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
-function App() {
-  return (
-    <div className="App">Hello!!!!!
-    </div>
-  );
-}
+import React from "react"
 
-export default App;
+export default class App extends React.Component {
+  // 꼭 여기에 state 전부 선언할 필요 없음. 나중에 다른곳에서 그냥 추가해도됨.
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    },6000);
+  }
+  render() {
+    const { isLoading} = this.state;
+    return (
+      <React.Fragment>
+        <div>{isLoading ? "Loading" : "We are ready"}</div>
+      </React.Fragment>
+    )
+  }
+}
